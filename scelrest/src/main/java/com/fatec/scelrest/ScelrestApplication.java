cc6ae65d818749fb0ca88b38788a2474b710fecb
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-06-10 08:23:43 -0300

    inclusao da funcao 2

--- scelrest/src/main/java/com/fatec/scelrest/ScelrestApplication.java
+++ scelrest/src/main/java/com/fatec/scelrest/ScelrestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // funcao1
+
 @SpringBootApplication
 public class ScelrestApplication {
 
